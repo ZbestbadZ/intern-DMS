8bad90fc5ee95846379452314b20993a91f7fdfc
relaxBear20 <long061020@gmail.com>
2020-09-23 10:38:35 +0700

    add layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    @include('includes.head')
+</head>
+<body>
+    <div id="app">
+        @include('includes.header')
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    <footer class="row">
+        @include('includes.footer')
+    </footer>
+</body>
+</html>
