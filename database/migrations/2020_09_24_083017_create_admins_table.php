10a9d2a06fa2fbaedb173d74dc11759b8265f6b7
Phan Huy Hoàng <phanhoang134@gmail.com>
2020-09-25 15:46:25 +0700

    Add column name for admins table

--- database/migrations/2020_09_24_083017_create_admins_table.php
+++ database/migrations/2020_09_24_083017_create_admins_table.php
@@ -15,9 +15,10 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name, 50');
+            $table->string('username, 50');
+            $table->string('email, 50')->unique();
+            $table->string('password, 50');
             $table->timestamps();
         });
     }
