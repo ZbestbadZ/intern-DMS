addc29ea2307bb9ecdbe07022784f579d8d26c54
relaxBear20 <long061020@gmail.com>
2020-09-28 10:01:20 +0700

    fix admin seeder

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -13,34 +13,12 @@ class AdminSeeder extends Seeder
     public function run()
     {
         DB::table('admins')->insert([
+            'name' => 'abc',
             'username'=> 'admin',
             'email'=> 'admin@gmail.com',
             'password'=> bcrypt('123'),
         ]);
-        DB::table('admins')->insert([
-            'username'=> 'admin1',
-            'email'=> 'admin1@gmail.com',
-            'password'=> bcrypt('123'),
-        ]);
-        DB::table('admins')->insert([
-            'username'=> 'admin2',
-            'email'=> 'admin2@gmail.com',
-            'password'=> bcrypt('123'),
-        ]);
-        DB::table('admins')->insert([
-            'username'=> 'admin3',
-            'email'=> 'admin3@gmail.com',
-            'password'=> bcrypt('123'),
-        ]);
-        DB::table('admins')->insert([
-            'username'=> 'admin4',
-            'email'=> 'admin4@gmail.com',
-            'password'=> bcrypt('123'),
-        ]);
-        DB::table('admins')->insert([
-            'username'=> 'admin5',
-            'email'=> 'admin5@gmail.com',
-            'password'=> bcrypt('123'),
-        ]);
+        
+       
     }
 }
