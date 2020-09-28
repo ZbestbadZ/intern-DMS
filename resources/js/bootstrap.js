9f2d47aa5d0738b8a1720e17dff72d7b611fc2b1
relaxBear20 <long061020@gmail.com>
2020-09-28 08:51:26 +0700

    add login scaffoding

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,8 +11,6 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-    require( '../../node_modules/datatables.net/js/jquery.dataTables.js' );
-    require( '../../node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js' );
 } catch (e) {}
 
 /**
