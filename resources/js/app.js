ea8fdaa506951173ad31dc4a7742e380d5e42e6b
relaxBear20 <long061020@gmail.com>
2020-10-12 10:58:28 +0700

    add moment

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,10 @@ import 'datatables.net-searchpanes-bs4';
 import 'datatables.net-select-bs4';
 import $ from 'jquery';
 window.$ = window.jQuery = $;
+var moment = require('moment');
+window.moment = moment;
 import 'datatables.net-bs4';
 import 'datatables.net-dt';
 import 'datatables.net';
+
+
