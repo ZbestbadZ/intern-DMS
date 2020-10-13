b551e9fd9c3398b1a7e49f20d5d7201f3091b74c
relaxBear20 <long061020@gmail.com>
2020-10-13 15:15:04 +0700

    ad link

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">DMS management</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <div class="row d-block">
+                    <div class="col"><span><a href="{{url('/admin/list_user')}}">Users</a></span></div>
+                        <div class="col"><span><a href="{{url('/pickup')}}">Pickup Users</a></span></div>
+                        <div class="col"><span><a href="{{url('/recommend')}}">Recomended Users</a></span></div>
+                        <div class="col"><span><a href="{{url('/sticker')}}">Stickers</a></span></div>
+                    </div>
                 </div>
             </div>
         </div>
