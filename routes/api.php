c5d9dcc4300fe8f4209d08e52b165193e82d288c
ZbestbadZ <phanhoang134@gmail.com>
2020-10-22 15:27:57 +0700

    route api for product and order

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,40 +17,44 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::group(['middleware'=>'auth:api'],function() {
-    Route::group(['prefix'=>'sticker'],function(){
-        Route::get('','StickerController@index');
-        Route::get('{id}','StickerController@get');
-        Route::patch('{id}','StickerController@update');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::group(['prefix' => 'sticker'], function () {
+        Route::get('', 'StickerController@index');
+        Route::get('{id}', 'StickerController@get');
+        Route::patch('{id}', 'StickerController@update');
         Route::post('', 'StickerController@store');
-        Route::delete('{id}','StickerController@destroy');
+        Route::delete('{id}', 'StickerController@destroy');
     });
-    Route::group(['prefix'=>'recommend'],function() {
-        Route::get('','RecommendController@index');
-        Route::get('/{id}','RecommendController@show');
+    Route::group(['prefix' => 'recommend'], function () {
+        Route::get('', 'RecommendController@index');
+        Route::get('/{id}', 'RecommendController@show');
     });
-    Route::group(['prefix'=>'pickup'],function(){
-        Route::get('','PickupController@index');
-      
-    });
-    
-    Route::group(['prefix'=>'masterData'],function(){
-        Route::get('job','MasterDataController@job');
+    Route::group(['prefix' => 'pickup'], function () {
+        Route::get('', 'PickupController@index');
     });
 
-    
+    Route::group(['prefix' => 'masterData'], function () {
+        Route::get('job', 'MasterDataController@job');
+    });
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('admin')->group(function() {
-    Route::get('list_user','UserManagementController@index');
+Route::prefix('admin')->group(function () {
+    Route::get('list_user', 'UserManagementController@index');
     Route::get('add_user', 'UserManagementController@add');
     Route::post('add_user', 'UserManagementController@store');
-    Route::get('edit_user/{id}','UserManagementController@edit');
+    Route::get('edit_user/{id}', 'UserManagementController@edit');
     Route::post('edit_user/{id}', 'UserManagementController@update');
-    Route::delete('{id}','UserManagementController@destroy');
+    Route::delete('{id}', 'UserManagementController@destroy');
     Route::get('{id}', 'UserManagementController@show');
+});
+
+Route::prefix('products')->group(function () {
+    Route::get('list', 'ProductController@index');
+});
+Route::prefix('orders')->group(function () {
+    Route::post('create', 'OrderController@store');
 });
--- routes/web.php
+++ routes/web.php
@@ -23,23 +23,24 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 
-    Route::group(['middleware' => 'auth'], function () {
-        Route::group(['prefix' => 'sticker'], function () {
-            Route::get('', 'StickerController@getIndex')->name('sticker.index');
-            Route::get('{id}/edit', 'StickerController@edit')->name('sticker.edit');
-            Route::get('/create', 'StickerController@getCreate')->name('sticker.create');
-        });
-        Route::group(['prefix' => 'recommend'], function () {
-            Route::get('', 'RecommendController@indexView')->name('recommend.index');
-        });
-        Route::group(['prefix' => 'pickup'], function () {
-            Route::get('', 'PickupController@getIndex')->name('pickup.index');
-        });
-        Route::group(['prefix'=>'admin'],function(){
-            Route::get('list_user', 'UserManagementController@getIndex')->name('admin.list_user');
-            Route::get('add_user', 'UserManagementController@add');
-            Route::get('edit_user/{id}', 'UserManagementController@edit')->name('admin.edit_user');
-        });
+Route::group(['middleware' => 'auth'], function () {
+    Route::group(['prefix' => 'sticker'], function () {
+        Route::get('', 'StickerController@getIndex')->name('sticker.index');
+        Route::get('{id}/edit', 'StickerController@edit')->name('sticker.edit');
+        Route::get('/create', 'StickerController@getCreate')->name('sticker.create');
     });
-    
-
+    Route::group(['prefix' => 'recommend'], function () {
+        Route::get('', 'RecommendController@indexView')->name('recommend.index');
+    });
+    Route::group(['prefix' => 'pickup'], function () {
+        Route::get('', 'PickupController@getIndex')->name('pickup.index');
+    });
+    Route::group(['prefix' => 'admin'], function () {
+        Route::get('list_user', 'UserManagementController@getIndex')->name('admin.list_user');
+        Route::get('add_user', 'UserManagementController@add');
+        Route::get('edit_user/{id}', 'UserManagementController@edit')->name('admin.edit_user');
+    });
+});
+Route::prefix('products')->group(function () {
+    Route::get('list', 'ProductController@index')->name('products.index');
+});
